39c96fe35c08d69f8d72875804ea843e64803632
Gr8ness21 <pmleechapman@gmail.com>
2019-04-26 10:32:14 -0400

    connected to local port

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 const express = require('express');
 const app = express();
+const methodOverride = require('method-override');
 
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
+
+// tells express handlebars is rendering the page
+app.set('view engine', 'hbs');
+
+// connecting to the port/view
+// displays when you go to local server - localhost:3000/
+app.get('/users', (req, res) =>{
+    res.send('Im in this bih!');
+});
+
+//creating boilerplate express code
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log("connected at:" + PORT)
+});
