af93f615fe3b350af3f33698a75fb7e6c6b642f6
Gr8ness21 <pmleechapman@gmail.com>
2019-04-30 09:28:25 -0400

    created functions to render teams in local host

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const methodOverride = require('method-override');
 
+//calling all established functions in respective APIs
+const teamApi = require('./api/teamApi.js');
+const playerApi = require('./api/playerApi.js');
+
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
@@ -10,8 +14,15 @@ app.set('view engine', 'hbs');
 
 // connecting to the port/view
 // displays when you go to local server - localhost:3000/
-app.get('/', (req, res) =>{
-    res.send('Im in this bih!');
+// app.get('/', (req, res) =>{
+//     res.send('Im in this bih!');
+// });
+
+app.get('/teams', (req, res) =>{
+    teamApi.getAllTeams()
+    .then(teams => {
+        res.render("teams/teams", {listOfTeams: teams});
+    });
 });
 
 //creating boilerplate express code
