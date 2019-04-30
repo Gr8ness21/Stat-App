672527640e6297ba902f1b87d0bf29f181278dee
Gr8ness21 <pmleechapman@gmail.com>
2019-04-30 15:34:06 -0400

    created player API functions

--- API/playerApi.js
+++ API/playerApi.js
@@ -13,16 +13,31 @@ const PlayerSchema = mongoose.Schema({
 // API that will take the "teams" collection in mongo
 let PlayerCollection = mongoose.model("Player", PlayerSchema);
 
+// Function to Create New players
 function createPlayer(newPlayer, playerId){
     newPlayer.playerId = playerId;
     return PlayerCollection.create(newPlayer);
 }
 
+// Function to Get all players
 function getAllPlayersByPlayerId(pId) {
-    return PlayerCollection.find({ playerId: pId})
+    return PlayerCollection.find({ playerId: pId});
 }
 
+// Function to get player by Id
+function getPlayerById(playerId) {
+    return PlayerCollection.findById(playerId);
+}
+
+//function to delete player by Id
+function deletePlayerById(playerId) {
+    return PlayerCollection.deleteOne({ _id: playerId });
+}
+
+// calls all established functions to be exported
 module.exports = {
     createPlayer,
-    getAllPlayersByPlayerId
+    getAllPlayersByPlayerId,
+    getPlayerById,
+    deletePlayerById
 };
