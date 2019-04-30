94598db6dd86273bfcdf60759d0917092e2a345f
Gr8ness21 <pmleechapman@gmail.com>
2019-04-30 12:06:43 -0400

    updated team API to full CRUD

--- API/teamApi.js
+++ API/teamApi.js
@@ -31,17 +31,17 @@ function deleteTeamById(teamId) {
     return TeamCollection.deleteOne({ _id: teamId });
 }
 
-// // Function to update Team
-// function updateTeamById(teamId, team) {
-//     return TeamCollection.updateOne({ _id: teamId }, team);
-//     //possible bug: not sure if it works
-// }
+// Function to update Team
+function updateTeamById(teamId, team) {
+    return TeamCollection.updateOne({ _id: teamId }, team);
+    //possible bug: not sure if it works
+}
 
 // calls all established functions to be exported
 module.exports = {
     getAllTeams,
     createNewTeams,
     getTeamById,
-    deleteTeamById
-    // updateTeamById
+    deleteTeamById,
+    updateTeamById
 };
