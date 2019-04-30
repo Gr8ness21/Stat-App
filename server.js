260db36cb357826986948a88e01801fdee15e574
Gr8ness21 <pmleechapman@gmail.com>
2019-04-30 12:17:40 -0400

    linked css page - full CRUD on team links

--- server.js
+++ server.js
@@ -12,12 +12,24 @@ app.use(methodOverride('_method'));
 // tells express handlebars is rendering the page
 app.set('view engine', 'hbs');
 
+// Linking CSS
+app.use('/public', express.static("public"))
+// app.use('style.css', express.static("style.css"));
+
+// Rendering home page
+app.get('/', (req, res) => {
+    res.render('home')
+ });
+
 // connecting to the port/view
 // displays when you go to local server - localhost:3000/
 // app.get('/', (req, res) =>{
 //     res.send('Im in this bih!');
 // });
 
+// -------------------------------
+// Team
+// -------------------------------
 app.get('/teams', (req, res) =>{
     teamApi.getAllTeams()
     .then(teams => {
@@ -25,10 +37,36 @@ app.get('/teams', (req, res) =>{
     });
 });
 
+// Posting a new team
 app.post('/teams', (req, res) =>{
     teamApi.createNewTeams(req.body)
     .then(() =>{
         res.render("teams/created");
+    });
+});
+
+// Deleting a team
+app.delete('/teams/:teamId', (req, res) =>{
+    teamApi.deleteTeamById(req.params.teamId)
+    .then(() => {
+        res.render("teams/deleted");
+    });
+});
+
+// grab a single team
+app.get('/teams/:teamId', (req, res) =>{
+    //gets team
+    teamApi.getTeamById(req.params.teamId)
+    .then(team => {
+        res.render("teams/team", {team});
+        });
+    });
+
+// Updating a team
+app.put('/teams/:teamId', (req, res) =>{
+    teamApi.updateTeamById(req.params.teamId, req.body)
+    .then(() => {
+        res.redirect("/teams");
     });
 });
 
