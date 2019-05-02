bd3ee7bb20d2f48f4c1140913fdd548501e48a65
Gr8ness21 <pmleechapman@gmail.com>
2019-05-02 06:53:36 -0400

    undid assisted changes

--- database/connection.js
+++ database/connection.js
@@ -1,28 +1,11 @@
 //connecting mongoose
 const mongoose = require('mongoose')
 
-// const dbConnection = process.env.MONGODB_URI || 'mongodb://localhost:27017/stat_app'
-// mongoose.connect(dbConnection, { useNewUrlParser: true})
-//   .then(() => {
-//     console.log("mongo is working");
-//   })
-if (process.env.MONGODB_URI) {
-    mongoose.connect(process.env.MONGODB_URI);
-} 
-else {
-    mongoose.connect('mongodb://localhost/stat_app', { useNewUrlParser: true })
-}
-mongoose.connection.on('error', function(err) {
-    console.error('MongoDB connection error: ' + err);
-    process.exit(-1);
-    }
-  );
-  mongoose.connection.once('open', function() {
-    console.log("Mongoose has connected to MongoDB!");
-  });
+const dbConnection = process.env.MONGODB_URI || 'mongodb://localhost:27017/stat_app'
+mongoose.connect(dbConnection, { useNewUrlParser: true})
+  .then(() => {
+    console.log("mongo is working");
+  })
+
 // export your mongoose connection
 module.exports = mongoose;
-
-// const mongoose = require('mongoose');
-
-// module.exports = mongoose;
